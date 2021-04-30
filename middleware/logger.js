8371508183713ea057b769950e10a1448e596135
Balarishi Saravanan <31584817+Noobynerd@users.noreply.github.com>
2021-04-30 09:37:46 +0530

    fixed typos

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,8 @@
 const logger = (req, res, next) => {
   console.log(
-    `${req.method} ${req.protocol}://${req.get("hotst")}.${req.originalUrl}`
+    `${req.method} ${req.protocol}://${req.get("host")}.${req.originalUrl}`
   );
   next();
 };
 
-module.exports = logger
+module.exports = logger;
