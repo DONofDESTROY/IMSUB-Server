4097b352a0ec6be23d77ac871f962db76b0aa266
DONofDESTROY <donofdestroy2000@gmail.com>
2021-05-06 23:35:38 +0530

    added sendEmail and ability to change password

--- controller/IMSUB-auth-controller.js
+++ controller/IMSUB-auth-controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/IMSUB-User');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/ErrorResponse');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 /**
  * @desc         login
@@ -82,10 +84,60 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // Get reset token
   const resetToken = user.getResetPasswordToken();
   await user.save({ validateBeforeSave: false });
-  res.status(200).json({
-    success: true,
-    data: user,
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`;
+  const message = `You are receiving this email because you (or someone else)
+	has requested the reset of password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+    res.status(200).json({ success: true, data: 'Email Sent' });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undifned;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
+});
+
+/**
+ * @desc         Reset password
+ * @route        PUT /api/v1/auth/resetpassword/:resettoken
+ * @access       Public
+ */
+
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
   });
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid token', 400));
+  }
+
+  // Set new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
